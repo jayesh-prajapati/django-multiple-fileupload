4f7da77722e9670a1470179c893e2cf2bacfb724
jayesh.prajapti <jayesh.prajapati@ia.ooo>
2018-10-15 15:20:57 +0530

    Created fileapp and included static, django templates folders.

---
+++ static/js/custom.js
@@ -0,0 +1,50 @@
+$(function () {
+  $(".js-upload-photos").click(function () {
+    $("#fileupload").click();
+  });
+
+  $("#fileupload").fileupload({
+    dataType: 'json',
+    done: function (e, data) {  
+      if (data.result.is_valid) {
+        $("#gallery tbody").prepend(
+          "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+        )
+      }
+    }
+  });
+
+});
+
+$(function () {
+
+  $(".js-upload-photos").click(function () {
+    $("#fileupload").click();
+  });
+
+  $("#fileupload").fileupload({
+    dataType: 'json',
+    sequentialUploads: true,  /* 1. SEND THE FILES ONE BY ONE */
+    start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
+      $("#modal-progress").modal("show");
+    },
+    stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
+      $("#modal-progress").modal("hide");
+    },
+    progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      var strProgress = progress + "%";
+      $(".progress-bar").css({"width": strProgress});
+      $(".progress-bar").text(strProgress);
+    },
+    done: function (e, data) {
+      if (data.result.is_valid) {
+        $("#gallery tbody").prepend(
+          "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+        )
+      }
+    }
+
+  });
+
+});
